d9eed2274c51c1aeaaefac6f889a16d430b45035
李超杰 <lichaojie@tzt.cn>
2026-02-04 16:34:59 +0800

    调整入口名称

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Stake from "./pages/stake/page";
 
-// Home Page 首页加载入口
-const Home = () => <Stake />;
+// Index Page 首页加载入口
+const Index = () => <Stake />;
 
-export default Home;
+export default Index;
